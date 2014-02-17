5061d2e8cf4850a71f81d4962b9278be3ebf40c5
James Legg <james.legg.conversocial.com>
2014-02-17 17:46:49 +0000

    install from duo security pkgs on debian/ubuntu

---
+++ recipes/install.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: duo_unix
+# Recipe:: install
+#
+
+if node['duo_unix']['from_source']
+  include_recipe 'duo_unix::source'
+else
+  case node['platform']
+  when 'debian', 'ubuntu'
+    lsb_codename = node['lsb']['codename']
+    platform = node['platform']
+    if node['duo_unix'][platform][lsb_codename]['repo_url']
+      apt_repository "duosecurity" do
+        uri node['duo_unix'][platform][lsb_codename]['repo_url']
+        distribution lsb_codename
+        components ['main']
+        key node['duo_unix'][platform][lsb_codename]['repo_gpg']
+      end
+
+      package "duo-unix"
+
+    else
+      # If no supported pacakge for this relase install from source
+      include_recipe 'duo_unix::source'
+    end
+  else
+      # If no pacakge for OS try and install from source
+      include_recipe 'duo_unix::source'
+  end
+end
---
+++ recipes/source.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: duo_unix
+# Recipe:: source
+#
+# Will install duo_security from source
+#
+case node[:platform]
+when "debian", "ubuntu"
+	package "libssl-dev" do
+		action :upgrade
+	end
+	package "make" do
+		action :upgrade
+	end
+end
+
+# Install duo_unix from source
+configure_options = node['duo_unix']['configure_options'].join(" ")
+version = node['duo_unix']['version']
+remote_file "#{Chef::Config[:file_cache_path]}/duo_unix-#{version}.tar.gz" do
+  source "#{node['duo_unix']['url']}#{version}.tar.gz"
+  checksum node['duo_unix']['checksum']
+  mode "0644"
+  notifies :run, "bash[build-and-install-duo_unix]"
+end
+
+bash "build-and-install-duo_unix" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOF
+  tar -xzvf duo_unix-#{version}.tar.gz
+  (cd duo_unix-#{version} && ./configure #{configure_options})
+  (cd duo_unix-#{version} && make && make install)
+  EOF
+  # Only build when notified that a file has been re-downloaded
+  action :nothing
+end
+
+
